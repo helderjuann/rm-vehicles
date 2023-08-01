28793e1eceab48a5dfdddc2d0a24862b002d683f
Heldinnn <helderjuango@gmail.com>
2023-08-01 18:44:02 -0300

    Minor Update

---
+++ script/functions.js
@@ -0,0 +1,34 @@
+$( function() {
+
+    var currentValue = 0;
+    var nowPrice = 0;
+    var maxPrice = 70000;
+    var isDrag = false;
+
+    $('.pointer-bar').mousedown(function() {
+        isDrag = true;
+    })
+
+    $(document).mouseup(function() {
+        isDrag = false;
+    })
+
+    $('.price-bar').mousemove(function(e) {
+        if (isDrag == true) {
+            var elBase = $(this);
+            var mouseX = e.pageX - elBase.offset().left;
+            if (mouseX < 0)
+                mouseX = 0;
+            if (mouseX > elBase.width())
+                mouseX = el.Base.width();
+            
+            $('.pointer-bar').css('left',(mouseX-13)+'px');
+            currentValue = (mouseX / elBase.width()) * 100;
+            $('.price-bar-fill').css('width',currentValue+'%');
+        }
+    })
+
+
+
+
+})
